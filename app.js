18bed9e4b42201f424926407795b0471c2ff1626
imanassi <imanassi@gmail.com>
2013-08-18 12:51:12 +0300

    adding db

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var User = require('./models/User.js');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -24,12 +25,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/form', function(req, res) {
+	fs.readFile('./form.html', function(error, content) {
+		if (error) {
+			res.writeHead(500);
+			res.end();
+		} else {
+			res.writeHead(200, {
+				'Content-Type' : 'text/html'
+			});
+			res.end(content, 'utf-8');
+		}
+	});
+});
+app.post('/signup', function(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	User.addUser(username, password, function(err, user) {
+		if (err){
+			throw err;
+		}
+		res.redirect('/form');
+	});
+});
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
